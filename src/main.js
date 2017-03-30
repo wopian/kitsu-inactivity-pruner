6cdd1e82b4fe9e0471036916ff9027dca297ab6b
James Harris <wopiancitizen123@gmail.com>
2017-03-30 22:26:48 +0100

    feat: display only inactive users

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import OAuth2 from 'client-oauth2'
 import JsonApi from 'devour-client'
 import input from 'input'
+import moment from 'moment'
 import { version } from '../package'
 // import { log, err, pe } from './util'
 import { env } from './env'
@@ -33,6 +34,21 @@ Kitsu.define('follow', {
   }
 })
 
+Kitsu.define('activity', {
+  time: ''
+})
+
+Kitsu.define('activityGroup', {
+  activity: {
+    jsonApi: 'hasOne',
+    type: 'activity'
+  },
+  activities: {
+    jsonApi: 'hasOne',
+    type: 'activities'
+  }
+}, { collectionPath: 'feeds/user_aggr' })
+
 const main = async () => {
   let { accessToken } = await auth.owner.getToken(username, password)
   Kitsu.headers['Authorization'] = `Bearer ${accessToken}`
@@ -50,6 +66,7 @@ const main = async () => {
   })
 
   let getFollows = async (offset) => {
+    // Get followed users
     let response = await Kitsu.findAll('follow', {
       fields: {
         users: 'name'
@@ -62,11 +79,27 @@ const main = async () => {
       },
       sort: '-created_at'
     })
+    // Display total user count
+    if (offset === 0) console.log(`\nFound ${await response.meta.count} users`)
     for (let user of await response) {
-      if (await input.confirm(`Do you want to unfollow ${user.followed.name}?`, { default: false })) {
-        console.log('If only deletion was implemented yet.')
+      // Get last activity from each followed user
+      let feed = await Kitsu.one('activityGroup', user.followed.id).get({
+        include: 'media,user',
+        page: { limit: 1 }
+      })
+      // Get the activity timestamp
+      let time = await moment(feed[0].activities.time)
+      // Skip if last activity was less than 60 days
+      if (await moment().diff(time, 'months') >= 6) {
+        // console.log(`\nhttps://kitsu.io/users/${user.followed.name}`)
+        console.log(`\n${await user.followed.name} was last active ${await time.fromNow()}`)
+        // Ask user for confirmation
+        if (await input.confirm(`Unfollow ${user.followed.name}?`, { default: false })) {
+          console.log('If only deletion was implemented yet.')
+        }
       }
     }
+    // Load next page if it exists
     if (await response.links.next) await getFollows(offset += 20)
   }
 
