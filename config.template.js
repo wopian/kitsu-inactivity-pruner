5ad2f898e111199914a77b2fd8bc4912b976947c
James Harris <wopian@wopian.me>
2018-09-07 01:48:42 +0100

    chore: remove babel dependencies
    BREAKING: Requires Node 10 or greater

--- config.template.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  USERNAME: '',
-  PASSWORD: '',
-  CLIENT_ID: '', // Leave blank
-  CLIENT_SECRET: '' // Leave blank
-}
---
+++ config.template.mjs
@@ -0,0 +1,6 @@
+export const config = {
+  USERNAME: '',
+  PASSWORD: '',
+  CLIENT_ID: '', // Leave blank
+  CLIENT_SECRET: '' // Leave blank
+}
